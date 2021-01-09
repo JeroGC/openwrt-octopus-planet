256649b5a5c5536ab633e10ce62acf1e06b1c58b
JeroGC <34290284+JeroGC@users.noreply.github.com>
2021-01-09 17:22:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/honwen/luci-app-aliddns package/luci-app-aliddns
+git clone https://github.com/xkstudio/luci-app-pptp-server package/luci-app-pptp-server
+cd package && git clone https://github.com/kenzok8/openwrt-packages && git clone https://github.com/kenzok8/small && cd ..
